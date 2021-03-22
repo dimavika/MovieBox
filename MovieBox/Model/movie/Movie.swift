7e1b74af8c51b76109e5bec9e790871b16b1c5d8
dimavi_ka <artes2222@yandex.ru>
2021-03-22 20:33:24 +0300

    Commit fix

--- MovieBox/Model/movie/Movie.swift
+++ MovieBox/Model/movie/Movie.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import Firebase
 
-class Movie {
+struct Movie: Equatable, Hashable {
     
     public var id: String
     public var title: String
@@ -15,18 +16,22 @@ class Movie {
     public var year: String
     public var country: String
     public var slogan: String
+    public var description: String
     public var imageUrl: String
     public var videoUrl : String
+    public var date: Timestamp
     
     
-    init(id: String, title: String, genre: String, year: String, country: String, slogan: String, imageUrl: String, videoUrl: String) {
+    init(id: String, title: String, genre: String, year: String, country: String, slogan: String, description: String, imageUrl: String, videoUrl: String, date: Timestamp) {
         self.id = id
         self.title = title
         self.genre = genre
         self.year = year
         self.country = country
         self.slogan = slogan
+        self.description = description
         self.imageUrl = imageUrl
         self.videoUrl = videoUrl
+        self.date = date
     }
 }
